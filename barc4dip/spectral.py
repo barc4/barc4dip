48bcbfed64ba3e53aba122c6bb528bc6eca22e88
Rafael CELESTRE <rafael.celestre@gmail.com>
2025-01-10 13:53:14 +0100

    feat: place holder for spectral analysis codes
     On branch dev
     Changes to be committed:
    	new file:   barc4dip/spectral.py

---
+++ barc4dip/spectral.py
@@ -0,0 +1,15 @@
+""" 
+Spectral analisys auxiliary functions
+"""
+
+__author__ = ['Rafael Celestre']
+__contact__ = 'rafael.celestre@synchrotron-soleil.fr'
+__license__ = 'CC BY-NC-SA 4.0'
+__copyright__ = 'Synchrotron SOLEIL, Saint Aubin, France'
+__created__ = '10/JAN/2025'
+__changed__ = '10/JAN/2025'
+
+# ****************************************************************************
+# ********************** speckles
+# ****************************************************************************
+
